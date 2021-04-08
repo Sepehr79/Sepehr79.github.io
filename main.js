66df4ce141389503d8b45bfe97628232af5b4b43
Sepehr79 <68536625+Sepehr79@users.noreply.github.com>
2021-04-08 19:15:57 +0430

    dark mode

---
+++ main.js
@@ -0,0 +1,28 @@
+$(function (){
+
+    $("#mode").prop("checked", true)
+
+    $("#mode").change(function (){
+        let tag = document.getElementsByTagName("section")
+        let footer = document.getElementsByTagName("footer").item(0)
+
+        if (this.checked) {
+            for (i = 0; i < tag.length; i++){
+                tag[i].style.backgroundColor = "black"
+                tag[i].style.color = "white"
+            }
+
+            footer.style.backgroundColor = "black"
+            footer.style.color = "white"
+
+        } else {
+            for (i = 0; i < tag.length; i++){
+                tag[i].style.backgroundColor = "#FFFFFF"
+                tag[i].style.color = "black"
+            }
+
+            footer.style.backgroundColor = "#FFFFFF"
+            footer.style.color = "black"
+        }
+    })
+})
